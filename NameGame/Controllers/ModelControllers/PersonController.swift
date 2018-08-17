a8c2f678ef1de30f76dce7aaa5b9824d51d93516
zfrew <zmfrew@gmail.com>
2018-08-16 18:19:42 -0600

    Set up model and model controller

---
+++ NameGame/Controllers/ModelControllers/PersonController.swift
@@ -0,0 +1,63 @@
+//
+//  PersonController.swift
+//  NameGame
+//
+//  Created by Zachary Frew on 8/16/18.
+//  Copyright © 2018 Zachary Frew. All rights reserved.
+//
+
+import Foundation
+
+class PersonController {
+    
+    // MARK: - URL Properties
+    static let baseURL = URL(string: "https://willowtreeapps.com/api/v1.0/profiles/")
+    
+    // MARK: - Instance Properties
+    var people: [Person] = []
+    
+    // MARK: - Instance Methods
+    static func retrieveAllPeople(completion: @escaping ([Person]?) -> Void) {
+        guard let url = baseURL else { completion(nil); return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                print("Error occurred retrieving data from API: \(error.localizedDescription).")
+                completion(nil)
+                return
+            }
+            
+            guard let data = data else { completion(nil); return }
+            
+            do {
+                
+                guard let resultsArray = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [AnyObject] else {
+                    print("Json data was not converted correctly.")
+                    completion(nil)
+                    return
+                }
+                
+                var people: [Person] = []
+                
+                for personDictionary in resultsArray {
+                    if let person = Person(personDictionary: personDictionary as! [String: Any]) {
+                        people.append(person)
+                    }
+                }
+                
+                completion(people)
+                
+            } catch {
+                print("Error occurred decoding JSON data: \(error.localizedDescription).")
+                completion(nil)
+                return
+            }
+            
+        }.resume()
+        
+    }
+    
+    
+    
+    
+}
---
+++ NameGame/Models/Person.swift
@@ -0,0 +1,51 @@
+//
+//  Person.swift
+//  NameGame
+//
+//  Created by Zachary Frew on 8/16/18.
+//  Copyright © 2018 Zachary Frew. All rights reserved.
+//
+
+import UIKit
+
+class Person {
+    
+    // MARK: - Key Constants
+    private let firstNameKey = "firstName"
+    private let lastNameKey = "lastName"
+    private let jobTitleKey = "jobTitle"
+    private let headshotKey = "headshot"
+    private let headshotURLKey = "url"
+    
+    // MARK: - Properties
+    let firstName: String
+    let lastName: String
+    let fullName: String
+    let jobTitle: String?
+    let headshotURL: String
+    
+    // MARK: - Initializers
+    init(firstName: String, lastName: String, jobTitle: String?, headshotURL: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.fullName = "\(firstName) \(lastName)"
+        self.jobTitle = jobTitle
+        self.headshotURL = headshotURL
+    }
+    
+    init?(personDictionary: [String: Any]) {
+        guard let firstName = personDictionary[firstNameKey] as? String,
+            let lastName = personDictionary[lastNameKey] as? String,
+            let jobTitle = personDictionary[jobTitleKey] as? String,
+            let headshotDictionary = personDictionary[headshotKey] as? [String: Any],
+            let headshotURL = headshotDictionary[headshotURLKey] as? String
+            else { return nil }
+        
+        self.firstName = firstName
+        self.lastName = lastName
+        self.fullName = "\(firstName) \(lastName)"
+        self.jobTitle = jobTitle
+        self.headshotURL = headshotURL
+    }
+    
+}
